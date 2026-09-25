6d030771afe98d3f3c7663d8eec2e997daa7a6ce
Wasswa Makubuya Stephen (Pheny) <wasswastephen24@gmail.com>
2026-09-25 14:19:40 +0300

    Initialize JavaFX desktop starter with Java 21

---
+++ desktop-client/src/main/java/com/medvora/desktopclient/Launcher.java
@@ -0,0 +1,9 @@
+package com.medvora.desktopclient;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
